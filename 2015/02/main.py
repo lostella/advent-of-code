b0e07176d3e81d47c9903e0359a3f5cfc2b4cbca
Lorenzo Stella <lorenzostella@gmail.com>
2021-11-29 22:45:33 +0000

    add second part

--- 2015/02/main.py
+++ 2015/02/main.py
@@ -13,6 +13,9 @@ def prod(iter):
 def paper_needed(dims):
     return 2 * sum(map(prod, combinations(dims, 2))) + prod(smallest_two(dims))
 
+def ribbon_needed(dims):
+    return 2 * sum(smallest_two(dims)) + prod(dims)
+
 def get_dims(line):
     return list(map(int, line.strip().split('x')))
 
@@ -20,3 +23,5 @@ if __name__ == "__main__":
     input_path = pathlib.Path(__file__).parent.resolve() / "input"
     with open(input_path) as fp:
         print(sum(map(paper_needed, map(get_dims, fp))))
+    with open(input_path) as fp:
+        print(sum(map(ribbon_needed, map(get_dims, fp))))

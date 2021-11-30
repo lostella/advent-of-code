91c11d1bc25b25e771269e3a69318ec5eeb16982
Lorenzo Stella <lorenzostella@gmail.com>
2021-11-30 12:06:29 +0000

    finish part one

--- 2015/05/main.py
+++ 2015/05/main.py
@@ -1,5 +1,10 @@
+import pathlib
+
+def pairs(s):
+    return zip(s[:-1], s[1:])
+
 def has_double(s):
-    for a, b in zip(s[:-1], s[1:]):
+    for a, b in pairs(s):
         if a == b:
             return True
     return False
@@ -7,3 +12,22 @@ def has_double(s):
 def count_vowels(s):
     return len([c for c in s if c in "aeiou"])
 
+def is_good(s):
+    if not has_double(s):
+        return False
+    if count_vowels(s) < 3:
+        return False
+    forbidden_pairs = [
+        ("a", "b"),
+        ("c", "d"),
+        ("p", "q"),
+        ("x", "y"),
+    ]
+    if not set(pairs(s)).isdisjoint(forbidden_pairs):
+        return False
+    return True
+
+if __name__ == "__main__":
+    input_path = pathlib.Path(__file__).parent.resolve() / "input"
+    with open(input_path) as fp:
+        print(len([line for line in fp if is_good(line.strip())]))

1684a924999077e6a481af3e1ba5abba00d2bb1f
Lorenzo Stella <lorenzostella@gmail.com>
2021-12-01 22:28:43 +0000

    simplify

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -6,7 +6,7 @@ def diff(data):
 if __name__ == "__main__":
     input_path = pathlib.Path(__file__).parent.resolve() / "input"
     with open(input_path) as fp:
-        depths = list(map(lambda l: int(l.strip()), fp))
+        depths = list(map(int, fp))
     print(sum(d > 0 for d in diff(depths)))
     depths3 = [sum(t) for t in zip(depths, depths[1:], depths[2:])]
     print(sum(d > 0 for d in diff(depths3)))

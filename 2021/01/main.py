d5e40736fa15b14555b7dc340beb9e7625481b2d
Lorenzo Stella <lorenzostella@gmail.com>
2021-12-01 10:19:38 +0000

    Update main.py

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -1,12 +1,12 @@
 import pathlib
 
 def diff(data):
-    return [b - a for a, b in zip(data,  data[1:])]
+    return (b - a for a, b in zip(data,  data[1:]))
 
 if __name__ == "__main__":
     input_path = pathlib.Path(__file__).parent.resolve() / "input"
     with open(input_path) as fp:
         depths = list(map(lambda l: int(l.strip()), fp))
-    print(len([d for d in diff(depths) if d > 0]))
+    print(sum(d > 0 for d in diff(depths)))
     depths3 = [sum(t) for t in zip(depths, depths[1:], depths[2:])]
-    print(len([d for d in diff(depths3) if d > 0]))
+    print(sum(d > 0 for d in diff(depths3)))

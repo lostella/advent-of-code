a68c26524cbf5cf940e13010680a78fa02df31fa
Lorenzo Stella <lorenzostella@gmail.com>
2021-12-01 22:50:26 +0000

    add templates

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -3,10 +3,12 @@ import pathlib
 def diff(data):
     return (b - a for a, b in zip(data,  data[1:]))
 
-if __name__ == "__main__":
-    input_path = pathlib.Path(__file__).parent.resolve() / "input"
+def main(input_path):
     with open(input_path) as fp:
         depths = list(map(int, fp))
     print(sum(d > 0 for d in diff(depths)))
     depths3 = [sum(t) for t in zip(depths, depths[1:], depths[2:])]
     print(sum(d > 0 for d in diff(depths3)))
+
+if __name__ == "__main__":
+    main(pathlib.Path(__file__).parent.resolve() / "input")
---
+++ templates/main.py
@@ -0,0 +1,7 @@
+import pathlib
+
+def main(input_path):
+    pass
+
+if __name__ == "__main__":
+    main(pathlib.Path(__file__).parent.resolve() / "input")

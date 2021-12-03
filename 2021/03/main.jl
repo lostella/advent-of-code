bc9696a8feb0c5b43b6b0fd134487964012e075f
Lorenzo Stella <lorenzostella@gmail.com>
2021-12-03 19:35:17 +0000

    simplify more

--- 2021/03/main.jl
+++ 2021/03/main.jl
@@ -2,22 +2,21 @@ to_decimal(x) = foldl((s, b) -> 2 * s + b, x)
 
 majority_bits(data) = round.(Int, sum(data) / length(data), Base.Rounding.RoundNearestTiesAway)
 
+function filter_data(data, criterion)
+    for k in 1:length(data[1])
+        m = criterion(data)
+        data = [d for d in data if d[k] == m[k]]
+        length(data) == 1 && break
+    end
+    return data
+end
+
 function main(input_path)
     data = [[c == '1' for c in line] for line in eachline(input_path)]
     m = majority_bits(data)
     println(to_decimal(m) * to_decimal(1 .- m))
-    data1 = data
-    for k in 1:length(data[1])
-        m = majority_bits(data1)
-        data1 = [d for d in data1 if d[k] == m[k]]
-        length(data1) == 1 && break
-    end
-    data2 = data
-    for k in 1:length(data[1])
-        m = 1 .- majority_bits(data2)
-        data2 = [d for d in data2 if d[k] == m[k]]
-        length(data2) == 1 && break
-    end
+    data1 = filter_data(data, d -> majority_bits(d))
+    data2 = filter_data(data, d -> 1 .- majority_bits(d))
     println(to_decimal(only(data1)) * to_decimal(only(data2)))
 end
 

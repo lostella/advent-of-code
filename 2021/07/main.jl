6167badaa38d279f63a2a83163603a60d41cb987
Lorenzo Stella <lorenzostella@gmail.com>
2021-12-07 09:50:49 +0100

    add 2021/07

---
+++ 2021/07/main.jl
@@ -0,0 +1,18 @@
+using Statistics
+
+function quadratic_fuel_cost(positions, target)
+    steps = abs.(positions .- target)
+    return sum(div.(steps .* (steps .+ 1), 2))
+end
+
+function main(input_path)
+    positions = parse.(Int, split(only(eachline(input_path)), ","))
+    optimal_position = round(Int, median(positions))
+    println(sum(abs.(positions .- optimal_position)))
+    mean_position = mean(positions)
+    println(minimum(quadratic_fuel_cost(positions, t) for t in [round.(Int, floor(mean_position)), round.(Int, ceil(mean_position))]))
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main(joinpath(dirname(@__FILE__), "input"))
+end

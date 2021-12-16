4263626e3aa12f1e907f8cabaea998a41dfdf711
Lorenzo Stella <lorenzostella@gmail.com>
2021-12-16 19:42:48 +0100

    simplify string parsing

--- 2021/16/main.jl
+++ 2021/16/main.jl
@@ -1,24 +1,4 @@
-_hex2bitstring = Dict(
-    '0' => "0000",
-    '1' => "0001",
-    '2' => "0010",
-    '3' => "0011",
-    '4' => "0100",
-    '5' => "0101",
-    '6' => "0110",
-    '7' => "0111",
-    '8' => "1000",
-    '9' => "1001",
-    'A' => "1010",
-    'B' => "1011",
-    'C' => "1100",
-    'D' => "1101",
-    'E' => "1110",
-    'F' => "1111",
-)
-
-hex2bits(c::Char) = _hex2bitstring[c] |> collect .|> bc -> parse(Bool, bc)
-hex2bits(s::String) = vcat(map(hex2bits, collect(s))...)
+hex2bits(s::String) = vcat((parse.(Int, collect(s), base=16) .|> (n -> digits(Bool, n, base=2, pad=4) |> reverse |> BitVector))...)
 bits2int(bits) = foldl((s, b) -> 2 * s + b, bits)
 
 struct Packet{type_id, P}

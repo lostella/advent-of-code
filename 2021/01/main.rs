f05899fa06adbd2b4f85cddb6a41fcc9ca884386
Lorenzo Stella <lorenzostella@gmail.com>
2022-08-17 12:35:57 +0200

    add more rust, format code

--- 2021/01/main.rs
+++ 2021/01/main.rs
@@ -3,21 +3,25 @@ use std::io::{self, prelude::*};
 use std::iter::zip;
 
 fn solve1(data: &Vec<u32>) -> usize {
-    zip(data[..data.len()-1].iter(), data[1..].iter()).filter(|(a, b)| a < b).count()
+    zip(data[..data.len() - 1].iter(), data[1..].iter())
+        .filter(|(a, b)| a < b)
+        .count()
 }
 
 fn solve2(data: &Vec<u32>) -> usize {
-    zip(data[..data.len()-3].iter(), data[3..].iter()).filter(|(a, b)| a < b).count()
+    zip(data[..data.len() - 3].iter(), data[3..].iter())
+        .filter(|(a, b)| a < b)
+        .count()
 }
 
 fn main() {
-    let file = fs::File::open("input").expect("file not found");
+    let file = fs::File::open("input").unwrap();
     let reader = io::BufReader::new(file);
     let values = reader
         .lines()
         .map(|line| line.unwrap().parse().unwrap())
         .collect::<Vec<u32>>();
-    
+
     println!("{}", solve1(&values));
     println!("{}", solve2(&values));
 }
---
+++ 2021/02/main.rs
@@ -0,0 +1,67 @@
+use std::fs;
+use std::io::{self, prelude::*};
+
+struct Submarine1 {
+    x: i32,
+    y: i32,
+}
+
+impl Submarine1 {
+    fn execute_command(&mut self, command: &str) {
+        let tokens = command.split_whitespace().collect::<Vec<&str>>();
+        let qty = tokens[1].parse::<i32>().unwrap();
+        match tokens[0] {
+            "up" => self.y -= qty,
+            "down" => self.y += qty,
+            "forward" => self.x += qty,
+            _ => (),
+        }
+    }
+}
+
+fn solve1(lines: &Vec<String>) -> i32 {
+    let mut submarine = Submarine1 { x: 0, y: 0 };
+    for line in lines {
+        submarine.execute_command(&line)
+    }
+    submarine.x * submarine.y
+}
+
+struct Submarine2 {
+    x: i32,
+    y: i32,
+    aim: i32,
+}
+
+impl Submarine2 {
+    fn execute_command(&mut self, command: &str) {
+        let tokens = command.split_whitespace().collect::<Vec<&str>>();
+        let qty = tokens[1].parse::<i32>().unwrap();
+        match tokens[0] {
+            "up" => self.aim -= qty,
+            "down" => self.aim += qty,
+            "forward" => {
+                self.x += qty;
+                self.y += self.aim * qty
+            }
+            _ => (),
+        }
+    }
+}
+
+fn solve2(lines: &Vec<String>) -> i32 {
+    let mut submarine = Submarine2 { x: 0, y: 0, aim: 0 };
+    for line in lines {
+        submarine.execute_command(&line)
+    }
+    submarine.x * submarine.y
+}
+
+fn main() {
+    let file = fs::File::open("input").unwrap();
+    let reader = io::BufReader::new(file);
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+
+    println!("{}", solve1(&lines));
+    println!("{}", solve2(&lines));
+}

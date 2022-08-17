8a28a579aac16f263cd75190cdd35dbfc6992849
Lorenzo Stella <lorenzostella@gmail.com>
2022-08-17 13:56:38 +0200

    make clippy happy

--- 2021/02/main.rs
+++ 2021/02/main.rs
@@ -9,8 +9,8 @@ struct Submarine1 {
 impl Submarine1 {
     fn execute_command(&mut self, command: &str) {
         let tokens = command.split_whitespace().collect::<Vec<&str>>();
-        let qty = tokens[1].parse::<i32>().unwrap();
-        match tokens[0] {
+        let qty = tokens.get(1).unwrap().parse::<i32>().unwrap();
+        match *tokens.first().unwrap() {
             "up" => self.y -= qty,
             "down" => self.y += qty,
             "forward" => self.x += qty,
@@ -22,7 +22,7 @@ impl Submarine1 {
 fn solve1(lines: &Vec<String>) -> i32 {
     let mut submarine = Submarine1 { x: 0, y: 0 };
     for line in lines {
-        submarine.execute_command(&line)
+        submarine.execute_command(line);
     }
     submarine.x * submarine.y
 }
@@ -36,13 +36,13 @@ struct Submarine2 {
 impl Submarine2 {
     fn execute_command(&mut self, command: &str) {
         let tokens = command.split_whitespace().collect::<Vec<&str>>();
-        let qty = tokens[1].parse::<i32>().unwrap();
-        match tokens[0] {
+        let qty = tokens.get(1).unwrap().parse::<i32>().unwrap();
+        match *tokens.first().unwrap() {
             "up" => self.aim -= qty,
             "down" => self.aim += qty,
             "forward" => {
                 self.x += qty;
-                self.y += self.aim * qty
+                self.y += self.aim * qty;
             }
             _ => (),
         }
@@ -52,7 +52,7 @@ impl Submarine2 {
 fn solve2(lines: &Vec<String>) -> i32 {
     let mut submarine = Submarine2 { x: 0, y: 0, aim: 0 };
     for line in lines {
-        submarine.execute_command(&line)
+        submarine.execute_command(line);
     }
     submarine.x * submarine.y
 }
@@ -60,7 +60,7 @@ fn solve2(lines: &Vec<String>) -> i32 {
 fn main() {
     let file = fs::File::open("input").unwrap();
     let reader = io::BufReader::new(file);
-    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+    let lines: Vec<String> = reader.lines().map(std::result::Result::unwrap).collect();
 
     println!("{}", solve1(&lines));
     println!("{}", solve2(&lines));

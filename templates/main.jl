51609d55b912c654a2e33fd8f684e53c382b4971
Lorenzo Stella <lorenzostella@gmail.com>
2022-12-01 20:41:42 +0100

    update templates

--- templates/main.jl
+++ templates/main.jl
@@ -1,5 +1,19 @@
+function read_data(path)
+
+end
+
+function solve1(data)
+    
+end
+
+function solve2(data)
+    
+end
+
 function main(input_path)
-    return
+    data = read_data(input_path)
+    println(solve1(data))
+    println(solve2(data))
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
--- templates/main.py
+++ templates/main.py
@@ -1,7 +1,18 @@
 import pathlib
 
-def main(input_path):
+def read_data(path):
     pass
+
+def solve1(data):
+    pass
+
+def solve2(data):
+    pass
+
+def main(path):
+    data = read_data(path)
+    print(solve1(data))
+    print(solve2(data))
 
 if __name__ == "__main__":
     main(pathlib.Path(__file__).parent.resolve() / "input")

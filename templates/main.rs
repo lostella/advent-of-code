0b4d7e9d1821ba1682ad281bee8f5186afdde34f
Lorenzo Stella <lorenzostella@gmail.com>
2026-01-11 12:02:52 +0100

    format

--- templates/main.rs
+++ templates/main.rs
@@ -2,11 +2,13 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let path = env::current_exe().expect("Error while getting executable path")
-        .parent().expect("Error while getting parent path")
+    let path = env::current_exe()
+        .expect("Error while getting executable path")
+        .parent()
+        .expect("Error while getting parent path")
         .join("input");
 
-    let content = fs::read_to_string(path).expect("Error while reading file");  
+    let content = fs::read_to_string(path).expect("Error while reading file");
 
     for line in content.lines() {
         println!("{}", line)
